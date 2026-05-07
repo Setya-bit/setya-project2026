7dcf81a09d84f01f58d98b8b4cb4c0d848cd1888
Setya-bit <241230027@unmuhpnk.ac.id>
2026-05-07 18:08:41 +0700

    database_comit

---
+++ app/Models/campaign.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class campaign extends Model
+{
+    protected $fillable = [
+        'title',
+        'description',
+        'target_donation',
+        'collected_donation',
+        'deadline',
+    ];
+}
+
--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,15 @@
 <?php
 
+use App\http\Controllers\CampaignController;
 use Illuminate\Support\Facades\Route;
+
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\KontakController;
 use App\Http\Controllers\ProfilController;
 
+// Rute untuk Halaman Home menggunakan Controller
 Route::get('/', [HomeController::class, 'index']);
-Route::get('/profil', function () {
-    return view('profil');
-});
+Route::get('/kontak', [KontakController::class, 'index']);
+Route::get('/profil', [ProfilController::class, 'index']);
 
-Route::get('/kontak', function () {
-    return view('kontak');
-});
+Route::resource('campaign', CampaignController::class);
